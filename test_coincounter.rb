0331bf3b6efdc292a41da9600a66c62bcf55fa45
Sheri Dyson <dysonsl@verizon.net>
2017-02-17 16:12:05 -0500

    41 = 1 of each

--- test_coincounter.rb
+++ test_coincounter.rb
@@ -16,7 +16,11 @@ class TestCoincounter < Minitest::Test
 		assert_equal({:quarter => 3, :penny => 3},coinchanger(78))
 	end
 
-	def test_3_100_in_4_quarters_out
+	def test_4_100_in_4_quarters_out
 		assert_equal({:quarter => 4},coinchanger(100))
+	end
+
+	def test_5_one_of_each_out
+		assert_equal({:quarter => 1, :dime => 1, :nickel => 1, :penny => 1},coinchanger(41))
 	end
 end
