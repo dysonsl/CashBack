0f9f841a875d0ff60b9f23d5f1ded61d5577c116
Sheri Dyson <dysonsl@verizon.net>
2017-02-17 17:16:30 -0500

    Example 3 from homework

--- test_coincounter.rb
+++ test_coincounter.rb
@@ -48,4 +48,7 @@ class TestCoincounter < Minitest::Test
 		assert_equal({:quarter => 1,:nickel=> 1, :penny => 1},coinchanger(31))
 	end
 
+	def test_12_example_three_from_homework
+		assert_equal({:quarter => 3,:dime => 1, :nickel=> 1, :penny => 3},coinchanger(93))
+	end
 end
