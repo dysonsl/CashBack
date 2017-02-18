85009a040ec53a73b2a6b6099ae5535e0488f297
Sheri Dyson <dysonsl@verizon.net>
2017-02-18 13:48:34 -0500

    Just Playing

--- app.rb
+++ app.rb
@@ -2,17 +2,19 @@ require 'sinatra'
 require_relative 'CashRegister.rb'
 
 get "/" do 
-	erb :register, :locals => {:result => "", :cost => "", :paid => "", :change_due => ""}
+	erb :register, :locals => {:result => "", :display_cost => "", :display_paid => "", :change_due => ""}
 end
 
 post "/cashback" do
 	cost = params[:amount_due].to_f
 	paid = params[:amount_paid].to_f
 
-	change_due = ((paid - cost).round(3))  #horrible float...rounds off weird on its own
+	change_due = "$ #{((paid - cost).round(3))}"  #horrible float...rounds off weird on its own
 
 	result = cash_register(cost, paid)
+	display_cost = "$ #{cost}"
+	display_paid = "$ #{paid}"
 
-	erb :register, :locals => {:result => result, :cost => cost, :paid => paid, :change_due => change_due}
+	erb :register, :locals => {:result => result, :display_cost => display_cost, :display_paid => display_paid, :change_due => change_due}
 
 end
